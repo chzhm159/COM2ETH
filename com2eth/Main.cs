6db822f2806147b50e5844ca02bcf8779c09d36b
chzhm159 <chzhm159@gmail.com>
2025-08-11 23:39:13 +0800

    Update Main.cs
    临时保存

--- com2eth/Main.cs
+++ com2eth/Main.cs
@@ -136,4 +136,28 @@ namespace com2eth
 
         }
     }
+
+/*
+apply from: "../gradle/java-project.gradle"
+apply from: "../gradle/java-codequality.gradle"
+apply from: "../gradle/guice-project.gradle"
+apply from: "../gradle/publishing-java.gradle"
+
+version = '1.0-SNAPSHOT'
+
+dependencies {
+  api project(':opentcs-api-injection')
+  api project(':opentcs-common')
+  api project(':opentcs-api-base')
+
+  testImplementation platform('org.junit:junit-bom:5.10.0')
+  testImplementation 'org.junit.jupiter:junit-jupiter'
+  implementation 'org.aspectj:aspectjrt:1.9.24'
 }
+
+test {
+  useJUnitPlatform()
+}
+*/
+}
+
